faf1f4897893fc130c1ad2c6fab86c4d94101914
olivi <olivi@ARKHANGELSK>
2016-06-09 21:23:44 +0200

    Add project files.

---
+++ DetectHololens/HololensStateTrigger.cs
@@ -0,0 +1,28 @@
+﻿using Windows.System.Profile;
+using Windows.UI.Xaml;
+
+namespace DetectHololens
+{
+    public class HololensStateTrigger : StateTriggerBase
+    {
+        private bool _needHololens;
+
+        public bool NeedHololens
+        {
+            get { return _needHololens; }
+            set
+            {
+                _needHololens = value;
+
+                if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Holographic")
+                {
+                    SetActive(value);
+                }
+                else
+                {
+                    SetActive(!value);
+                }
+            }
+        }
+    }
+}
